05c2e47e256dbf8b643096df3abb000a069e8474
Kevin Pang <kpanghmc@gmail.com>
2014-09-26 12:35:42 -0700

    Only show results if deckUrl is provided

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,34 +1,35 @@
 app.controller("SearchController", function($scope, $location) {
-	$scope.search = function() {
-	  var tappedout = $("#tappedout").val();
-	  $location.search("tappedout", tappedout);
-	}
+  var deckUrl = getParameterByName("deckUrl");
+  if (deckUrl) {
+    $scope.deckUrl = deckUrl;
 
-  if (getParameterByName("tappedout") == null) {
-    return;
+  	$scope.creatures = [
+  		{ name: "Aetherling" },
+  	  { name: "Jin-Gitaxias, Core Augur" },
+  	  { name: "Spellskite" }
+  	];
+
+  	$scope.nonCreatures = [
+  	  { name: "Counterspell" },
+  	 	{ name: "Hinder" },
+  	  { name: "Forbid" }
+  	];
+
+  	$scope.lands = [
+  		{ name: "Island" },
+  		{ name: "Mutavault" },
+  		{ name: "Command Tower"}
+  	];
+
+  	$scope.uniques = [
+  		{ name: "Cancel" },
+  		{ name: "Remand" },
+  		{ name: "Spell Crumple"}
+  	]
   }
   
-	$scope.creatures = [
-		{ name: "Aetherling" },
-	  { name: "Jin-Gitaxias, Core Augur" },
-	  { name: "Spellskite" }
-	];
-	
-	$scope.nonCreatures = [
-	  { name: "Counterspell" },
-	 	{ name: "Hinder" },
-	  { name: "Forbid" }
-	];
-	
-	$scope.lands = [
-		{ name: "Island" },
-		{ name: "Mutavault" },
-		{ name: "Command Tower"}
-	];
-	
-	$scope.uniques = [
-		{ name: "Cancel" },
-		{ name: "Remand" },
-		{ name: "Spell Crumple"}
-	]
+  $scope.search = function() {
+	  var deckUrl = $("#deckUrl").val();
+	  $location.search("deckUrl", deckUrl);
+	}
 });
