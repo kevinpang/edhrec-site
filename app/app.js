71ebb52cfc24f29dfaeeb4efcac93ef99677442c
Kevin Pang <kpanghmc@gmail.com>
2014-09-26 11:46:37 -0700

    Add CSS styling and search box

--- app/app.js
+++ app/app.js
@@ -2,14 +2,23 @@ var app = angular.module("app", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
 	$routeProvider
-			.when("/",
-					{
-						templateUrl: "app/templates/main.html"
-					})
-			.when("/suggestions/tappedout/:id",
+			.when("/", {})
+			.when("/suggestions",
 					{
 						controller: "SuggestionsController",
 						templateUrl: "app/templates/suggestions.html"
 					})
 		  .otherwise({ redirectTo: "/" });
+});
+
+$(document).ready(function() {
+  $("#search").submit(function() {
+    var tappedout = $("#tappedout").val();
+    var reload = window.location.href.indexOf("suggestions") > 0;
+    
+    window.location.href = "#/suggestions?tappedout=" + tappedout;
+    if (reload) {
+      window.location.reload();      
+    }
+  });
 });
