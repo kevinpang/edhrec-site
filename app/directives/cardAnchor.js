c62c56a5c93f05d0a7469d593eab6438bbc0564b
Kevin Pang <kpanghmc@gmail.com>
2014-09-26 23:54:53 -0700

    Minor tweaks

--- app/directives/cardAnchor.js
+++ app/directives/cardAnchor.js
@@ -1,10 +1,10 @@
-app.directive("card", function() {
+app.directive("cardAnchor", function() {
   return {
     restrict: "E",
     scope: {
       name: "@"
     },
-    templateUrl: "app/directives/card.html",
+    templateUrl: "app/directives/cardAnchor.html",
     controller: function($scope, cardService) {
       $scope.getCardUrl = function(name) {
         return cardService.getCardUrl(name);
