4a6945e594e1814d56d8fafcb936193b9b7c503e
Kevin Pang <kpanghmc@gmail.com>
2014-09-27 13:03:54 -0700

    Remove unnecessary scope variable

--- app/controllers/recommendationsController.js
+++ app/controllers/recommendationsController.js
@@ -1,7 +1,6 @@
 app.controller("RecommendationsController", function($scope, $location, cardService, recommendationService) {
   var deckUrl = $location.search().deckUrl;
   if (deckUrl) {
-    $scope.deckUrl = deckUrl;
     $scope.recommendations = recommendationService.getRecommendations(deckUrl);
   }
 
