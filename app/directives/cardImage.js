d69c73f7b6725dc89f41e5a46cab42836d2ee4fd
Kevin Pang <kpanghmc@gmail.com>
2014-09-26 23:45:19 -0700

    Create cardImage directive

---
+++ app/directives/cardImage.js
@@ -0,0 +1,14 @@
+app.directive("cardImage", function() {
+  return {
+    restrict: "E",
+    scope: {
+      name: "@"
+    },
+    templateUrl: "app/directives/cardImage.html",
+    controller: function($scope, cardService) {
+      $scope.getCardImage = function(name) {
+        return cardService.getCardImage(name);
+    	};      
+    }
+  }
+});
