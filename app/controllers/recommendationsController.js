8ee196f3e19b11e395ca1886ccb1fb1eaaf1f253
Kevin Pang <kpanghmc@gmail.com>
2014-09-28 16:49:08 -0700

    Show loading bar while retrieving recommendations

--- app/controllers/recommendationsController.js
+++ app/controllers/recommendationsController.js
@@ -1,11 +1,14 @@
 app.controller("RecommendationsController", function($scope, $location, cardService, recommendationService) {
   var deckUrl = $location.search().deckUrl;
   if (deckUrl) {
+    $scope.loading = true;
     recommendationService.getRecommendations(deckUrl)
         .then(function(data) {
+          $scope.loading = false;
           $scope.deckUrl = deckUrl;
           $scope.recommendations = data;
         }).catch(function() {
+          $scope.loading = false;
           $scope.error = "An error occurred while generating recommendations. Please try again";
         });
   }
