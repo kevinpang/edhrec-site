8d5f394cec97a1c34efcd06497e2aa41d227ec4d
Kevin Pang <kpanghmc@gmail.com>
2014-09-29 11:56:41 -0700

    Minor formatting change

--- app/app.js
+++ app/app.js
@@ -2,15 +2,13 @@ var app = angular.module("app", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
   $routeProvider
-      .when("/",
-          {
-            controller: "HomeController",
-            templateUrl: "app/templates/home.html"
-          })
-      .when("/recommendations",
-          {
-            controller: "RecommendationsController",
-            templateUrl: "app/templates/recommendations.html"
-          })
+      .when("/", {
+        controller: "HomeController",
+        templateUrl: "app/templates/home.html"
+      })
+      .when("/recommendations", {
+        controller: "RecommendationsController",
+        templateUrl: "app/templates/recommendations.html"
+      })
       .otherwise({ redirectTo: "/" });
 });
