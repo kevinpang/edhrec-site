79d327bf038166a46b785901d5e300ebabf89d7b
Kevin Pang <kpanghmc@gmail.com>
2014-09-28 23:01:37 -0700

    Remove unused function

--- app/directives/cardAnchor.js
+++ app/directives/cardAnchor.js
@@ -9,11 +9,7 @@ app.directive("cardAnchor", function() {
     controller: function($scope, cardService) {
       $scope.getCardUrl = function(name) {
         return cardService.getCardUrl(name);
-      };
-
-      $scope.getCardImage = function(name) {
-        return cardService.getCardImage(name);
-      };      
+      };     
     }
   }
 });
