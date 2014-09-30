60ccac62a31f0707b6d07743db752e3df541915a
Kevin Pang <kpanghmc@gmail.com>
2014-09-29 23:18:27 -0700

    Refactor recommendationService callback

--- app/services/recommendationService.js
+++ app/services/recommendationService.js
@@ -3,6 +3,7 @@ var EDHREC_API_URL = "http://edhrec.com/rec";
 var API_REF = "kevin";
 var LAND_TYPE = "Land";
 var CREATURE_TYPE = "Creature";
+var MAX_TOP_RECOMMENDATIONS = 12;
 var MAX_RECOMMENDATIONS_PER_CATEGORY = 15;
 
 app.service("recommendationService", function($http, $q) {
@@ -25,51 +26,51 @@ app.service("recommendationService", function($http, $q) {
     return $.inArray(CREATURE_TYPE, card.card_info.types) > -1;
   }
   
+  this.parseResponse_ = function(data) {
+    var recs = data.recs.sort(this.cardSortFunction_);
+    var cuts = data.cuts; // Deliberately not sorting since most have the same score.    
+    var top = [];
+    var creatures = [];
+    var nonCreatures = [];
+    var lands = [];
+    
+    for (var i = 0; i < recs.length; i++) {
+      var card = recs[i];
+      var land = this.isLand_(card);
+      var creature = this.isCreature_(card);
+      
+      if (top.length < MAX_TOP_RECOMMENDATIONS && !land) {
+        top.push(card);
+      } else {
+        if (land) {
+          lands.push(card);
+        } else if (creature) {
+          creatures.push(card);
+        } else {
+          nonCreatures.push(card);
+        }
+      }
+    }
+    
+    return {
+      top: top,
+      creatures: creatures.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY),
+      nonCreatures: nonCreatures.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY),
+      lands: lands.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY),
+      cuts: cuts.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY)
+    };
+  }
+  
   this.getRecommendations = function(deckUrl) {
     var deferred = $q.defer();
-
     if (!this.isValidDeckUrl_(deckUrl)) {
       deferred.reject("Invalid deck URL.");
       return deferred.promise;
     }
     
     var url = EDHREC_API_URL + "?to=" + deckUrl + "&ref=" + API_REF;
-    $http.get(url).then($.proxy(function(result) {
-      var recs = result.data.recs.sort(this.cardSortFunction_);
-      
-      var recommendations = {
-        top: [],
-        creatures: [],
-        nonCreatures: [],
-        lands: [],
-        // Deliberately not sorting cuts since a lot of them end up with the same score.
-        cuts: result.data.cuts
-      };
-            
-      for (var i = 0; i < recs.length; i++) {
-        var card = recs[i];
-        var land = this.isLand_(card);
-        var creature = this.isCreature_(card);
-        
-        if (recommendations.top.length < 12 && !land) {
-          recommendations.top.push(card);
-        } else {
-          if (land) {
-            recommendations.lands.push(card);
-          } else if (creature) {
-            recommendations.creatures.push(card);
-          } else {
-            recommendations.nonCreatures.push(card);
-          }
-        }
-      }
-      
-      recommendations.creatures = recommendations.creatures.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY);
-      recommendations.nonCreatures = recommendations.nonCreatures.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY);
-      recommendations.lands = recommendations.lands.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY);
-      recommendations.cuts = recommendations.cuts.slice(0, MAX_RECOMMENDATIONS_PER_CATEGORY);
-      
-      deferred.resolve(recommendations);
+    $http.get(url).then($.proxy(function(result) {    
+      deferred.resolve(this.parseResponse_(result.data));
     }, this), function(error) {
       deferred.reject("Error generating recommendations. Status code: " + error.status);
     });
