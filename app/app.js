87573848fd32070464fca807b7898f5feba711aa
Kevin Pang <kpanghmc@gmail.com>
2014-10-04 01:08:15 -0700

    Test

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ app.config(function ($routeProvider) {
   $routeProvider
       .when("/", {
         controller: "HomeController",
-        templateUrl: "app/templates/home.html"
+        templateUrl: "app/home/home.html"
       })
       .when("/recommendations", {
         controller: "RecommendationsController",
