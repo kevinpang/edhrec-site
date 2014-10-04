c2f40a3fd4ebe96257714e6fee43e97faebc09f9
Kevin Pang <kpanghmc@gmail.com>
2014-10-04 03:19:41 -0700

    Group files by feature, not type

--- app/card_image/card_image_directive.js
+++ app/card_image/card_image_directive.js
@@ -4,7 +4,7 @@ app.directive("cardImage", function() {
     scope: {
       name: "@"
     },
-    templateUrl: "app/templates/cardImage.html",
+    templateUrl: "app/card_image/card_image.html",
     controller: function($scope, cardService) {
       $scope.getCardImage = function(name) {
         return cardService.getCardImage(name);
