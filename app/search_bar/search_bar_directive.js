8111b391827d95514ffb611fd6e7d163a0e2db6b
Kevin Pang <kpanghmc@gmail.com>
2014-10-05 15:40:52 -0700

    Remove unnecessary scope field

--- app/search_bar/search_bar_directive.js
+++ app/search_bar/search_bar_directive.js
@@ -1,9 +1,6 @@
 app.directive("searchBar", function() {
   return {
     restrict: "E",
-    scope: {
-      name: "@"
-    },
     templateUrl: "app/search_bar/search_bar.html",
     controller: function($scope, $location, settings) {
       $scope.sampleDeckUrl = settings.SAMPLE_DECK_URL;
