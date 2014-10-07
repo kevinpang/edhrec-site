6d59dcec8c7ded33ac1de6c0a7439903b2dc9b1d
Kevin Pang <kpanghmc@gmail.com>
2014-10-06 23:55:00 -0700

    Minor refactoring and UI cleanup

--- app/recommendations/recommendations_controller.js
+++ app/recommendations/recommendations_controller.js
@@ -2,37 +2,23 @@ app.controller("RecommendationsController", function(
     $scope, $location, $timeout, recommendationService) {
   $scope.loading = true;
   var query = $location.search().q;  
+  var getRecommendationsPromise = null;
   
   if (query.toLowerCase().indexOf("tappedout") > -1) {
     if (query.indexOf("http") < 0) {
       query = "http://" + query;
     }
-    
-    recommendationService.getDeckRecommendations(query)
-        .then($.proxy(function(recommendations) {
-          $scope.loading = false;
-          $scope.deckUrl = query;
-          $scope.recommendations = recommendations;
-          this.updateMoreRecommendationsUi_();
-        }, this)).catch(function(errorMessage) {
-          $scope.loading = false;
-          $scope.error = errorMessage;
-        });
+    $scope.deckUrl = query;
+    getRecommendationsPromise = recommendationService.getDeckRecommendations(query);
   } else {
-    recommendationService.getCommanderRecommendations(query)
-        .then($.proxy(function(recommendations) {
-          $scope.loading = false;
-          $scope.commander = query;
-          $scope.recommendations = recommendations;
-          this.updateMoreRecommendationsUi_();
-        }, this)).catch(function(errorMessage) {
-          $scope.loading = false;
-          $scope.error = errorMessage;
-        });
+    $scope.commander = query;
+    getRecommendationsPromise = recommendationService.getCommanderRecommendations(query);
   }
   
-  // Optimizes "More recommendations" layout and sets up hover handlers.
-  this.updateMoreRecommendationsUi_ = function() {
+  getRecommendationsPromise.then($.proxy(function(recommendations) {
+    $scope.loading = false;
+    $scope.recommendations = recommendations;
+
     // Don't run post processing until the next digest cycle so that the
     // sections have had a chance to render.
     $timeout(function() {
@@ -40,12 +26,15 @@ app.controller("RecommendationsController", function(
       moreRecommendations.masonry({
         itemSelector: "more-recommendations"
       });
-      
+
       $("#moreRecommendations card-anchor").mouseenter(function() {
         window.console.log("mouseenter");
         var img = $(this).next().find("img");
         img.attr("src", img.attr("lazy-src"));
       });
     }, 1000);
-  }
+  }, this)).catch(function(errorMessage) {
+    $scope.loading = false;
+    $scope.error = errorMessage;
+  });
 });
