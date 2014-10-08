ae86e616a5fbd99d3bc403d65f539973ff8437e9
Kevin Pang <kpanghmc@gmail.com>
2014-10-08 01:10:59 -0700

    Lazy load "recent decks" card images

--- app/recent_decks/recent_decks_directive.js
+++ app/recent_decks/recent_decks_directive.js
@@ -2,9 +2,18 @@ app.directive("recentDecks", function() {
   return {
     restrict: "E",
     templateUrl: "app/recent_decks/recent_decks.html",
-    controller: function($scope, recentDecksService) {
+    controller: function($scope, $timeout, recentDecksService) {
       recentDecksService.getRecentDecks(20).then(function(recentDecks) {
         $scope.recentDecks = recentDecks;
+        
+        // Don't run post processing until the next digest cycle so that the
+        // sections have had a chance to render.
+        $timeout(function() {
+          $("#recent_decks card-anchor").mouseenter(function() {
+            var img = $(this).next().find("img");
+            img.attr("src", img.attr("lazy-src"));
+          });
+        }, 1000);
       }, function() {
         // Do nothing.
       });
