9790f7aa9215185dbec94f20187962e0bb0af7b6
Kevin Pang <kpanghmc@gmail.com>
2014-10-08 00:15:49 -0700

    Fix card image/links that have Æ in the name

--- app/card/card_service.js
+++ app/card/card_service.js
@@ -1,9 +1,15 @@
 app.service("cardService", function() {
   this.getCardUrl = function(name) {
+    name = this.sanitizeName_(name);
     return "http://gatherer.wizards.com/Pages/Card/Details.aspx?name=" + name;
   };
   
   this.getCardImage = function(name) {
+    name = this.sanitizeName_(name);
     return "http://gatherer.wizards.com/Handlers/Image.ashx?name=" + name + "&type=card&.jpg";
   };
+  
+  this.sanitizeName_ = function(name) {
+    return name.replace("\u00C6", "Ae");
+  }
 });
