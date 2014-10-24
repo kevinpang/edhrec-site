dd08316592e6441311a3ce2808f9b9b9acaa3f1f
Kevin Pang <kpanghmc@gmail.com>
2014-10-23 23:35:57 -0700

    Accidentally deleted sample deck url from config

--- app/shared/config_module.js
+++ app/shared/config_module.js
@@ -1,5 +1,6 @@
 var config = {
-  SAMPLE_COMMANDER: "Rhys the Redeemed"
+  SAMPLE_COMMANDER: "Rhys the Redeemed",
+  SAMPLE_DECK_URL: "http://tappedout.net/mtg-decks/30-09-14-rhys-the-redeemed/"
 };
 
 if (window.location.hostname.indexOf("staging.edhrec.com") > -1) {
